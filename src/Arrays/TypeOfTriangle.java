c208bc1748f1122487c853618f26330703e474a3
RT <rohithtaninki01@gmail.com>
2025-05-19 23:31:33 +0530

    Traingle Commit

--- src/Arrays/TypeOfTriangle.java
+++ src/Arrays/TypeOfTriangle.java
@@ -4,22 +4,27 @@ public class TypeOfTriangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = new int[] { 9, 4, 9 };
+		int[] a = new int[] { 8,4,4 };
 		System.out.println(triangleType(a));
 	}
 
 	public static String triangleType(int[] nums) {
         String a = "";
-        if(nums[0] + nums[1] == 2*nums[2]){
-            a = "equilateral";
-        }
-        else{
-        	if(nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[1]){
+        
+        
+        if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]) {
+        	if(nums[0] == nums[1] && nums[1] == nums[2]){
+                a = "equilateral";
+            }
+        	else if(nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[1] || nums[0] == nums[2]){
         		a = "isosceles ";
             }
-        	else{
+        	else {
         		a = "scalene";
         	}
+        }
+        else {
+        	a = "none";
         }
         return a;
         }
