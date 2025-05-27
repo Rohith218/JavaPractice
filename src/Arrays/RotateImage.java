52a02dbd5a308b51a5c2bbb78cce4ce3f4559047
RT <rohithtaninki01@gmail.com>
2025-05-27 20:58:05 +0530

    Commiting changes

--- src/Arrays/RotateImage.java
+++ src/Arrays/RotateImage.java
@@ -8,6 +8,7 @@ public class RotateImage {
 	}
 	public void rotate(int[][] matrix) {
         int n = matrix.length;
+        
        for (int i = 0; i < n; i++) {
            for (int j = i + 1; j < n; j++) {
                // swap matrix[i][j] and matrix[j][i]
