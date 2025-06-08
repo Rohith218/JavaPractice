fe02e4dd19d842978502699729ca9db9b1bfa5f4
RT <rohithtaninki01@gmail.com>
2025-06-08 22:11:24 +0530

    Pattern commit

--- src/Patterns/RightTriangle.java
+++ src/Patterns/RightTriangle.java
@@ -4,11 +4,23 @@ public class RightTriangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		printRight(5);
+		printRightrev(5);
 	}
 	public static void printRight(int n)
 	{
 		for(int i =0;i<n;i++) {
+			for(int j = 0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void printRightrev(int n)
+	{
+		for(int i =0;i<n;i++) {
+			for(int k =n-i-1;k>0;k--) {
+				System.out.print(" ");
+			}
 			for(int j = 0;j<=i;j++) {
 				System.out.print("*");
 			}
