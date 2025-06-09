699cb41bc8be6c26d5d3461f2ca885a82bce4701
RT <rohithtaninki01@gmail.com>
2025-06-09 23:34:44 +0530

    halfDiamond commit

--- src/Patterns/RightTriangle.java
+++ src/Patterns/RightTriangle.java
@@ -4,7 +4,8 @@ public class RightTriangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		printRightrev(5);
+//		printRightrev(5);
+		halfDiamond(5);
 	}
 	public static void printRight(int n)
 	{
@@ -21,7 +22,28 @@ public class RightTriangle {
 			for(int k =n-i-1;k>0;k--) {
 				System.out.print(" ");
 			}
+			for(int j = 0;j<i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void halfDiamond(int n)
+	{
+		for(int i =0;i<n;i++) {
+			for(int k =n-i-1;k>0;k--) {
+				System.out.print(" ");
+			}
 			for(int j = 0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i =n-1;i>0;i--) {
+			for(int k =0;k<=n-i-1;k++) {
+				System.out.print(" ");
+			}
+			for(int j = 0;j<i;j++) {
 				System.out.print("*");
 			}
 			System.out.println();
