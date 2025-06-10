bf1891890efbcd4de6209e8e2fdd4982639952d0
RT <rohithtaninki01@gmail.com>
2025-06-10 22:07:56 +0530

    fullDiamond commit

--- src/Patterns/RightTriangle.java
+++ src/Patterns/RightTriangle.java
@@ -5,45 +5,66 @@ public class RightTriangle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		printRightrev(5);
-		halfDiamond(5);
+		fullDiamond(5);
 	}
-	public static void printRight(int n)
-	{
-		for(int i =0;i<n;i++) {
-			for(int j = 0;j<=i;j++) {
+
+	public static void printRight(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
-	public static void printRightrev(int n)
-	{
-		for(int i =0;i<n;i++) {
-			for(int k =n-i-1;k>0;k--) {
+
+	public static void printRightrev(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int k = n - i - 1; k > 0; k--) {
 				System.out.print(" ");
 			}
-			for(int j = 0;j<i;j++) {
+			for (int j = 0; j < i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
-	public static void halfDiamond(int n)
-	{
-		for(int i =0;i<n;i++) {
-			for(int k =n-i-1;k>0;k--) {
+
+	public static void halfDiamond(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int k = n - i - 1; k > 0; k--) {
 				System.out.print(" ");
 			}
-			for(int j = 0;j<=i;j++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		for(int i =n-1;i>0;i--) {
-			for(int k =0;k<=n-i-1;k++) {
+		for (int i = n - 1; i > 0; i--) {
+			for (int k = 0; k <= n - i - 1; k++) {
 				System.out.print(" ");
 			}
-			for(int j = 0;j<i;j++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void fullDiamond(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int k = n - i - 1; k > 0; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = n - 2; i >= 0; i--) {
+			for (int k = n - i - 1; k > 0; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * i + 1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
